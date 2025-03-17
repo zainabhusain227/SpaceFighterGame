6742adbd8168309a40ef2a31475fe84f10dfaf79
zainabhusain227 <41973371+zainabhusain227@users.noreply.github.com>
2025-03-17 11:23:36 -0400

    variable script created

---
+++ Assets/Scenes/VariableScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int x;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
